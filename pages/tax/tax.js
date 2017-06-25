5e573f78fce07cc9fea7b72843a054059fa2b2f4
SunRich <13257001077@126.com>
2017-06-25 18:03:07 +0800

    个税完成

--- pages/tax/tax.js
+++ pages/tax/tax.js
@@ -2,33 +2,49 @@ Page({
     data: {
         wage: 0,
         beginMoney: 0,
-        tax:0
+        tax: 0,
+        criticalAarray: [0, 1500, 4500, 9000, 35000, 55000, 80000],
+        taxRatesArray: [0, 0.03, 0.10, 0.20, 0.25, 0.30, 0.35, 0.45],
+        taxArray: []
     },
     onLoad: function (e) {
-        var tax=this.getTax(e.wage,e.beginMoney);
-        console.log(tax);
         this.setData({
             beginMoney: e.beginMoney,
             wage: e.wage,
-            tax:tax
+            tax: this.getTax(e.wage, e.beginMoney)
         });
-        console.log(e);
     },
     getTax: function (salary, beginMoney) {
-        var criticalAarray = [0, 1500, 4500, 9000, 35000, 55000, 80000];
-        var taxRatesArray = [0, 0.03, 0.10, 0.20, 0.25, 0.30, 0.35, 0.45];
-        var tax = 0;
+        var criticalAarray = this.data.criticalAarray;
+        var taxRatesArray = this.data.taxRatesArray;
+        var taxs = 0;
         var needTax = salary - beginMoney;
+        var taxArray = [];
         if (needTax > 0) {
             for (var i = 1; i < criticalAarray.length; i++) {
+                var tax = 0;
                 if (needTax >= criticalAarray[i]) {
-                    tax = tax + (criticalAarray[i] - criticalAarray[i - 1]) * taxRatesArray[i];
+                    tax = (criticalAarray[i] - criticalAarray[i - 1]) * taxRatesArray[i];
+                    taxs += tax;
+                    taxArray.push(tax);
+                    if (i == criticalAarray.length - 1) {
+                        tax = (needTax - criticalAarray[i]) * taxRatesArray[i + 1];
+                        taxs += tax;
+                        taxArray.push(tax);
+                    }
                 } else {
-                    tax = tax + (needTax - criticalAarray[i - 1]) * taxRatesArray[i];
+                    tax = (needTax - criticalAarray[i - 1]) * taxRatesArray[i];
+                    taxs += tax;
+                    taxArray.push(tax);
+                    console.log(criticalAarray[i]);
+                    console.log(taxRatesArray[i]);
                     break;
                 }
             }
         }
-        return tax;
+        this.setData({
+            taxArray: taxArray
+        });
+        return taxs;
     }
 })
